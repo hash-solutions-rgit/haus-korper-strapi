586969279e82210d51eccdb2e663b4c4740b4059
sjana7797 <sjana7797@gmail.com>
2025-01-03 13:30:24 +0530

    adding supabase

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,34 @@
-export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+export default ({ env }) => [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http:"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "lh3.googleusercontent.com", // google avatars
+            "platform-lookaside.fbsbx.com", // facebook avatars
+            "dl.airtable.com", // strapi marketplace,
+            "market-assets.strapi.io",
+            env("SUPABASE_API_URL"),
+          ],
+          "media-src": ["'self'", "data:", "blob:", env("SUPABASE_API_URL")],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
 ];
